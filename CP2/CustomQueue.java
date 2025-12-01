4bfab19f5ade92ae58e0b9f2d0f584206b81f23c
crisvm01 <cvindas10827@ufide.ac.cr>
2025-12-01 13:42:09 -0600

    Caso evaluado 2

---
+++ CP2/CustomQueue.java
@@ -0,0 +1,57 @@
+/*Estudiante Cristina Vindas Mendez*/
+public class CustomQueue {
+
+    private QueueNode _head;
+
+    public CustomQueue() {
+        _head = null;
+    }
+
+    public void enqueue(int index) {
+        var node = new QueueNode(index);
+
+        if(_head == null){
+            _head = node;
+        }
+        else{
+            QueueNode curr = _head;
+            while(curr.getNext() != null){
+                curr = curr.getNext();
+            }
+            curr.setNext(node);
+        }
+    }
+
+    public int dequeue() {
+        var index = -1;
+
+       if(_head != null){
+        index = _head.getIndex();
+        _head = _head.getNext();
+       }
+
+        return index;
+    }
+
+    public int getSize() {
+        var size = 0;
+
+        QueueNode curr = _head;
+        while (curr != null){
+            size++;
+            curr = curr.getNext();
+        }
+
+        return size;
+    }
+
+    public String getIndexes() {
+        var builder = new StringBuilder();
+        for (var index = dequeue(); index > -1; index = dequeue()) {
+            builder.append(" " + index);
+        }
+        return builder.toString();
+    }
+}
+
+
---
+++ CP2/CustomStack.java
@@ -0,0 +1,48 @@
+public class CustomStack {
+
+    private StackNode _head;
+
+    public CustomStack() {
+        _head = null;
+    }
+
+    public void push(String word) {
+        var node = new StackNode(word);
+
+        node.setNext(_head);
+
+        _head = node;
+    }
+
+    public String pop() {
+        String word = null;
+
+        if(_head != null){
+            word = _head.getWord();
+            _head = _head.getNext();
+        }
+
+        return word;
+    }
+
+    public int size() {
+        var length = 0;
+
+        StackNode curr = _head;
+        while (curr != null){
+            length++;
+            curr = curr.getNext();
+        }
+
+        return length;
+    }
+
+    public String getWords() {
+        var builder = new StringBuilder("(" + size() + ") [");
+        for (var word = pop(); word != null; word = pop()) {
+            builder.append(" " + word);
+        }
+        builder.append(" ]");
+        return builder.toString();
+    }
+}
